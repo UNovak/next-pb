9df690d2f89f1e813876fa2d6f670c0b79e425c8
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-05 01:31:50 +0200

    feat: add placholders for auth routes

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      sign in
+    </div>
+  )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const SignUp = () => {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      sign up
+    </div>
+  )
+}
+
+export default SignUp
