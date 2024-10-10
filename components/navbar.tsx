263dc2e9871a1d8e67f9f0d1051c4dff9c050939
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-10 04:30:05 +0200

    fix: remove pocketbase data from session storage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
   const handleSignOut = async () => {
     const res = await signOut()
     localStorage.removeItem('auth-session')
+    localStorage.removeItem('pocketbase_auth')
     update()
   }
 
